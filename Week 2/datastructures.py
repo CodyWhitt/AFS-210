13c728360bba8971506656f7f36bf07ff95f07fe
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2023-04-02 17:58:05 -0500

    Corrected submission

--- Week 2/datastructures.py
+++ Week 2/datastructures.py
@@ -1,35 +1,32 @@
-Data1 = [7, False, "Apple", True, 7, 98.6]
-Data2 = ("July", 4, 8, "Tango", 4.3, "Bingo")
-Data3 = {"A", 7, -1, 3.14, True}
-# The data was presented incorrectly in the assignment
-# Data4 = ["name" = "Joe", "age" = 26, "active" = True, "hourly_wage" = 14.75]
-# This is the correct format to have it store properly
+# tuple
+Data1 = (7, False, "Apple", True, 7, 98.6)
+
+# set
+Data2 = {"July", 4, 8, "Tango", 4.3, "Bingo"}
+
+# list
+Data3 = ["A", 7, -1, 3.14, True, 7]
+
+# Dictionary
 Data4 = {"name": "Joe", "age": 26, "active": True, "hourly_wage": 14.75}
 
-# DataSet1 
+# # DataSet1
 print(len(Data1))
 print(Data1[3])
 print(Data1.count(7))
 
 # DataSet2
-import random
-random_index = random.randint(0, len(Data2)-1)
-Data2_list = list(Data2)
-removed_item = Data2_list.pop(random_index)
-Data2_list.insert(random_index, "Alpha")
-Data2 = tuple(Data2_list)
+Data2.pop()
+Data2.add("Alpha")
 print(Data2)
 
-# DataSet3
-# Some editing had to be done here to meet expected code output as it needed to be stored
-# in reverse and not just printed as the steps in the assignment asked
-Data3 = list(Data3)
-Data3.reverse()
+# # DataSet3
+Data3 = Data3[::-1]
 Data3[1] = 'B'
-Data3 = set(Data3)
+Data3.pop()
 print(Data3)
 
-# DataSet4
+# # DataSet4
 Data4["active"] = False
 Data4["address"] = "123 West Main Street"
 weekly_salary = Data4["hourly_wage"] * 40
